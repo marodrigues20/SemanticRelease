dcf1f2e100952bf6e1a081cfc1134f931bad5c20
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-01 21:06:58 +0100

    feat: Test a Semantic-Release github action

---
+++ .github/workflows/release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    branches: ["master"],
+    repositoryUrl: "https://github.com/marodrigues20/react-app",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                asserts: [
+                    { path: "build.zip", label: "Build" }
+                ],
+            },
+        ],
+    ],
+};
