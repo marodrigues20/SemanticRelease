4bc2e649e78caf9dae3880bcf0a3f86f2bd0f50a
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-06 21:41:33 +0100

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/marodrigues20/SemanticRelease",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/git"
+      ]
+}
