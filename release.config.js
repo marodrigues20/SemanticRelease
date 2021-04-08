69c2a91fe9d8504c20e23f188f7c6b8d3aebfb4a
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-08 19:21:51 +0100

    feat: second feature # => v2.0.0-beta.2 on @beta

--- release.config.js
+++ release.config.js
@@ -6,13 +6,6 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/github",
-            {
-                assets: [
-                    { path: "SemanticRelease-*.jar", label: "Jar File" }
-                ]
-            }
-        ]
+        "@semantic-release/github"
     ]
 }
