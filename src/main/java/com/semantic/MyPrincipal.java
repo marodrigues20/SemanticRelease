c710847f880c614aa8e799429523e989db81cb54
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-08 18:29:41 +0100

    feat: first feature \n\n BREAKING CHANGE: it breaks something # => v2.0.0-beta.1 on @beta

--- src/main/java/com/semantic/MyPrincipal.java
+++ src/main/java/com/semantic/MyPrincipal.java
@@ -3,6 +3,8 @@ package com.semantic;
 public class MyPrincipal {
 
     public static void main(String[] args) {
-        System.out.println("Lets see it!!!  Google");
+        System.out.println("Lets see it!!!  Google_1");
+        System.out.println("Lets see it!!!  Google_2");
+        System.out.println("Lets see it!!!  Google_3");
     }
 }
