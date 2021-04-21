580b8900c90d5ac346ccd87e8d42840081da9b29
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-21 13:13:52 +0100

    feat: Conventional PR Action

--- src/main/java/com/semantic/MyPrincipal.java
+++ src/main/java/com/semantic/MyPrincipal.java
@@ -4,5 +4,6 @@ public class MyPrincipal {
 
     public static void main(String[] args) {
         System.out.println("Lets see it!!!  Google_1");
+        System.out.println("Lets see it!!!  Google_2");
     }
 }
