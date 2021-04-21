0f3ff2deda5e51002b7215ae051a281a06b151e7
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-04-21 13:16:40 +0100

    feat[SP-XPTO]: Conventional PR Action

--- src/main/java/com/semantic/MyPrincipal.java
+++ src/main/java/com/semantic/MyPrincipal.java
@@ -5,5 +5,6 @@ public class MyPrincipal {
     public static void main(String[] args) {
         System.out.println("Lets see it!!!  Google_1");
         System.out.println("Lets see it!!!  Google_2");
+        System.out.println("Lets see it!!!  Google_3");
     }
 }
